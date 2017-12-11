ae4427f190d450300a973e8089c6059e4e06ded4
Young Li <ylli2@illinois.edu>
2017-12-11 16:00:18 -0600

    Rename to client.go

---
+++ client.go
@@ -0,0 +1,72 @@
+/*
+ * Implementation of the client in Path ORAM by Stefanov et. al.
+ */
+package oram2pc
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+/*
+ * The client
+ */
+type Client struct {
+	stash []Block
+	pos map[uint]uint
+}
+
+/*
+ * Pretty print function for a client
+ *
+ * Prints the stash and various runtime statistics
+ */
+func (c *Client) Pp() {
+	// print blocks
+	for i := 0; i < len(c.stash); i++ {
+		fmt.Printf("{")
+
+		for j := 0; j < len(c.stash[i].mem); j++ {
+			fmt.Printf(" [%b]", c.stash[i].mem[j])
+		}
+
+		fmt.Printf(" }\n")
+	}
+
+	fmt.Println(c.pos)
+}
+
+/*
+ * Initialize a Client
+ *
+ * Returns a new Client with params:
+ *   N: number of blocks outsourced to server
+ *   L: height of binary tree
+ *   S: size of client's stash in blocks
+ *   B: Number of bytes in each block
+ */
+func Init_client(N uint, L uint, S uint, B uint) *Client {
+	c := &Client{stash: make([]Block, S, S), pos: make(map[uint]uint)}
+
+	// initialize stash as all zeroes
+	for i := 0; i < len(c.stash); i++ {
+		var j uint;
+		for j = 0; j < B; j++ {
+			c.stash[i].mem = make([]byte, B, B)
+		}
+	}
+
+	// initialize pos map as random values
+	// create cryptographically secure shuffling of leaves
+	random_leaves := rand.Perm(2 << L)
+
+	// assign each block with a unique random leaf
+	var i uint
+	for i = 0; i < N; i++ {
+		// not all leaves will be used if N < 2^L but that's okay
+		c.pos[i] = uint(random_leaves[i]);
+	}
+
+	return c
+}
+
