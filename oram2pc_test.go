ef9e864e5e3fb1358d5f58c420ad3fcd273d090d
Young Li <ylli2@illinois.edu>
2017-12-18 01:09:38 -0600

    Split utils_test.go away

--- oram2pc_test.go
+++ oram2pc_test.go
@@ -9,25 +9,13 @@ import (
 )
 
 func Test_pathoram(t *testing.T) {
-	c := Init_client(60, 6, 16, 4)
+	c := Init_client(6, 256 / 8, 4, 4)
 	c.Pp()
 
-	s := Init_server(64, 4)
+	s := Init_server(6, 256 / 8, 4, 4 * 1024)
 	fmt.Println(s.dir)
+
+	s.get_path(4)
 }
 
-func Test_utils(t *testing.T) {
-	a := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	b := Hex2b64(a)
-	fmt.Printf("a: %s\n", a)
-	fmt.Printf("b: %s\n", b)
 
-	c := "1c0111001f010100061a024b53535009181c"
-	d := "686974207468652062756c6c277320657965"
-	fmt.Printf("%s\nXOR\n%s is:\n%s\n", c, d, XOR_hex(c, d))
-
-	e := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-	Solve_char_xor(e)
-
-	fmt.Printf("%s\n", GenAlphanumString(20))
-}
